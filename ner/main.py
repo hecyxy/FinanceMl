38749f5c86d44781226d25220463349c346d1e21
hecy <hecy.hecy@lieluobo.com>
2019-07-08 18:31:08 +0800

    init

--- ner/main.py
+++ ner/main.py
@@ -8,7 +8,7 @@ import numpy as np
 # B-ORG  公司
 # B-LOC 地点
 model, (vocab, chunk_tags) = bilstm_crf.create_model(train=False)
-predict_text = '苏州杰艾人力资源职位分析师上海市'#'中华人民共和国国务院总理周恩来在外交部长陈毅的陪同下，连续访问了埃塞俄比亚等非洲10国以及阿尔巴尼亚'
+predict_text = '苏州杰艾人力资源职位分析师'#'中华人民共和国国务院总理周恩来在外交部长陈毅的陪同下，连续访问了埃塞俄比亚等非洲10国以及阿尔巴尼亚'
 str, length = process_data.process_data(predict_text, vocab)
 model.load_weights('ner/model/search.h5')
 raw = model.predict(str)[0][-length:]
